a9286131b7018381c183078fb0057e2d90442713
shengrong <shengrong_88@hotmail.com>
2020-12-07 10:20:14 +0800

    pcmob3-project1 initiate, task 1 (slide 61)

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const name = ["Adam", "Bob", "Claire", "Dinah"];
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{name[0]}</Text>
+      <Text>{name[1]}</Text>
+      <Text>{name[2]}</Text>
+      <Text>{name[3]}</Text>
       <StatusBar style="auto" />
     </View>
   );
